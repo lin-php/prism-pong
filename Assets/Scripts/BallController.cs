d7e5d7b77d9f3684e2754ae9b8d9455e471e6f70
lin-php <mann.philipp93@gmail.com>
2026-04-12 11:59:38 +0200

    calculate ballbounce direction based on hit position

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -7,12 +7,45 @@ public class BallController : MonoBehaviour
 
     private Rigidbody2D rb;
 
-    
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // calculate bounce direction based on hit position
+
+        if (collision.gameObject.CompareTag("linkerPaddle"))
+        {
+            float paddleY = collision.gameObject.transform.position.y;
+
+            float hitOffset = transform.position.y - paddleY;
+
+            Vector2 directionHit = new Vector2(+1f, hitOffset);
+
+            directionHit = directionHit.normalized;
+
+            rb.linearVelocity = directionHit * speed;
+        }
+
+        if (collision.gameObject.CompareTag("rechterPaddle"))
+        {
+            float paddleY = collision.gameObject.transform.position.y;
+
+            float hitOffset = transform.position.y - paddleY;
+
+            Vector2 directionHit = new Vector2(-1f, hitOffset);
+
+            directionHit = directionHit.normalized;
+
+            rb.linearVelocity = directionHit * speed;
+        }
+    }
     void Start()
     {
-        // Set initial direction and apply velocity to the ball
+        // set initial direction and apply velocity to the ball
 
-        rb = GetComponent<Rigidbody2D>();
         float spawnY = Random.Range(-1f, 1f);
 
         Vector2 direction = new Vector2(-1, spawnY);
@@ -20,12 +53,6 @@ public class BallController : MonoBehaviour
         direction = direction.normalized;
 
         rb.linearVelocity = direction * speed;
-        
-    }
-
-    
-    void Update()
-    {
         
     }
 }
