4e5eeec97120523480d68c7bbe156efbe6205b4f
lin-php <mann.philipp93@gmail.com>
2026-04-12 11:11:24 +0200

    simple enemy paddle AI

---
+++ Assets/Scripts/AIPaddleController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class AIPaddleController : MonoBehaviour
+{
+    [SerializeField] private float speed = 6f;
+
+    [SerializeField] private GameObject ball;
+
+    
+    void Update()
+    {
+        float pos = transform.position.y;
+
+        float ballY = ball.transform.position.y;
+
+        pos = Mathf.MoveTowards(pos, ballY, speed * Time.deltaTime);
+
+        float clampedY = Mathf.Clamp(pos, -4.5f, 4.5f);
+
+        transform.position = new Vector2 (transform.position.x, clampedY);    
+
+    }
+}
