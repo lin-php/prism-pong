84395b73b4b84a880a5b2e3e34cc6f5adfaacac5
lin-php <mann.philipp93@gmail.com>
2026-04-14 20:56:19 +0200

    WIP: refactor ball system, add dynamic spawning and AI ball tracking groundwork

--- Assets/Scripts/AIPaddleController.cs
+++ Assets/Scripts/AIPaddleController.cs
@@ -1,19 +1,24 @@
+
 using UnityEngine;
+using System.Collections.Generic;
+
 
 public class AIPaddleController : MonoBehaviour
 {
-    [SerializeField] private float speed = 6f;
 
-    [SerializeField] private GameObject ball;
+    [SerializeField] private float speed = 6f;
     [SerializeField] private float offsetRange;
     [SerializeField] private float offsetChangeInterval = 5f;
     [SerializeField] private float deadzone = 0.2f;
     [SerializeField] private float offsetSmoothSpeed = 1f;
-    
+
+    private List<GameObject> balls;
 
     private float currentOffset;
     private float targetOffset;
-    
+
+    private GameObject currentBall;
+
     private float timer;
 
     private void Start()
@@ -21,6 +26,11 @@ public class AIPaddleController : MonoBehaviour
         RandomOffset();
         currentOffset = targetOffset;
         timer = offsetChangeInterval;
+    }
+
+    public void SetBalls(List<GameObject> activeBalls)
+    {
+        balls = activeBalls;
     }
 
     private void RandomOffset()
@@ -45,8 +55,18 @@ public class AIPaddleController : MonoBehaviour
         }
 
         // take current position and ball height
+
         float pos = transform.position.y;
-        float ballY = ball.transform.position.y;
+
+        if (balls == null || balls.Count == 0)
+        {
+            return;
+
+        }
+ 
+        currentBall = balls[0];
+        
+        float ballY = currentBall.transform.position.y;
 
         // movement
         float difference = (ballY + currentOffset) - pos;
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,26 +1,33 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private BallController ballController;
+   
     [SerializeField] private TextMeshProUGUI playerScoreText;
     [SerializeField] private TextMeshProUGUI aiScoreText;
     [SerializeField] private GameObject ball1Prefab;
     [SerializeField] private GameObject ball2Prefab;
     [SerializeField] private float ball2Spawntimer = 5f;
+    [SerializeField] private GameObject AiPaddle;
 
     private int playerScore;
     private int aiScore;
     private float timerball2;
     private bool ball2isSpawned = false;
+    private AIPaddleController aIPaddleController;
+
+    private List<GameObject> activeBalls = new List<GameObject>();  
 
     // add points if goaled
-
     private void Start()
     {
+        aIPaddleController = AiPaddle.GetComponent<AIPaddleController>();
+
         timerball2 = 0f;
         UpdateScoreUI();
+        Ball1Instantiate();
     }
 
     private void Update()
@@ -32,16 +39,28 @@ public class GameManager : MonoBehaviour
             GameObject ball2 = Instantiate(ball2Prefab);
             BallController ball2Controller = ball2.GetComponent<BallController>();
             ball2Controller.SpawnBall();
+            activeBalls.Add(ball2);
+
+            aIPaddleController.SetBalls(activeBalls);
 
             ball2isSpawned = true;
         }
+    }
+
+    private void Ball1Instantiate()
+    {
+        GameObject ball1 = Instantiate(ball1Prefab);    
+        BallController ball1Controller = ball1.GetComponent<BallController>();  
+        ball1Controller.SpawnBall();
+        activeBalls.Add(ball1); 
     }
 
     public void AddPlayerPoint()
     {
         playerScore++;
         UpdateScoreUI();
-        ballController.SpawnBall();
+        ClearBalls();
+        Ball1Instantiate();
         timerball2 = 0f;
         ball2isSpawned = false;
     }
@@ -50,7 +69,8 @@ public class GameManager : MonoBehaviour
     {
         aiScore++;
         UpdateScoreUI();
-        ballController.SpawnBall();
+        ClearBalls();
+        Ball1Instantiate();
         timerball2 = 0f;
         ball2isSpawned = false;
     }
@@ -65,10 +85,21 @@ public class GameManager : MonoBehaviour
     // reset score and ball position
     public void RestartGame()
     {
+        ClearBalls();
         playerScore = 0;
         aiScore = 0;
         UpdateScoreUI();
-        ballController.SpawnBall();
+        Ball1Instantiate();
+    }
+
+    private void ClearBalls()
+    {
+        foreach (GameObject ball in activeBalls) 
+        {
+            Destroy(ball);
+        }
+
+        activeBalls.Clear();
     }
 
 }
