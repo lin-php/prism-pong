28df053b340b6ea9a7747da45de5cc302794b197
lin-php <mann.philipp93@gmail.com>
2026-06-09 16:48:22 +0200

    Finalized 1.0 release version
    - Reduced custom cursor size
    - Fixed an animation feedback bug

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -28,7 +28,7 @@ public class PauseMenu : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
         {
             if (!isPaused)
             {
